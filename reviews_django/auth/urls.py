a3d5e1b14a423c08490ea630497bb3f713cdbb73
Shaquille Johnson <sjohns25@nd.edu>
2016-07-14 15:56:41 -0400

    changes that relate to login using allatuh rather than redirects

--- reviews_django/auth/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url, include
-from auth import views
-
-urlpatterns = [
-    url(r'^', views.OsfAuthorizationCode.as_view()),
-]
--- reviews_django/peerreviews/models.py
+++ reviews_django/peerreviews/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User, Group
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialAccount
+import hashlib
 
 import datetime
 
@@ -49,3 +52,26 @@ class submissionevals(models.Model):
     def total(self):
         return self.premise + self.research + self.style
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+
+    def __unicode__(self):
+        return "{}'s profile".format(self.user.username)
+
+    class Meta:
+        db_table = 'user_profile'
+
+    def account_verified(self):
+        if self.user.is_authenticated:
+            result = EmailAddress.objects.filter(email=self.user.email)
+            if len(result):
+                return result[0].verified
+        return False
+
+    def profile_image_url(self):
+        fb_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='osf')
+        if len(fb_uid):
+            return "http://graph.facebook.com/{}/picture?width=40&height=40".format(fb_uid[0].uid)
+        return "http://www.gravatar.com/avatar/{}?s=40".format(hashlib.md5(self.user.email).hexdigest())
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
